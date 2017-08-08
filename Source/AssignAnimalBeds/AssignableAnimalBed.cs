3cb9318bc4368dca48fad68d9c2299b21631c605
Aaron C Robinson <robinson.aaron.c@qq.com>
2017-08-08 13:38:08 -0400

    Hospital Bed
    Added ability to assign hospital beds.

--- Source/AssignAnimalBeds/AssignableAnimalBed.cs
+++ Source/AssignAnimalBeds/AssignableAnimalBed.cs
@@ -1,11 +1,12 @@
-﻿using Harmony;
-using RimWorld;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
 using UnityEngine;
 using Verse;
+using Harmony;
+using RimWorld;
 
 namespace AssignAnimalBeds
 {
@@ -20,6 +21,19 @@ namespace AssignAnimalBeds
 
             if (!this.def.building.bed_humanlike && base.Faction == Faction.OfPlayer)
             {
+                yield return new Command_Toggle
+                {
+                    defaultLabel = "CommandBedSetAsMedicalLabel".Translate(),
+                    defaultDesc = "CommandBedSetAsMedicalDesc".Translate(),
+                    icon = ContentFinder<Texture2D>.Get("UI/Commands/AsMedical", true),
+                    isActive = (() => this.Medical),
+                    toggleAction = delegate
+                    {
+                        this.Medical = !this.Medical;
+                    },
+                    hotKey = KeyBindingDefOf.Misc2
+                };
+
                 yield return new Command_Action
                 {
                     // NOTE: this should come from somewhere else...
@@ -75,7 +89,19 @@ namespace AssignAnimalBeds
             StringBuilder stringBuilder = new StringBuilder();
             stringBuilder.Append(base.GetInspectString());
             stringBuilder.AppendLine();
-            if (this.PlayerCanSeeOwners)
+
+            /*if (this.PlayerCanSeeOwners)
+            {
+                stringBuilder.AppendLine("ForColonistUse".Translate());
+            }*/
+
+            if (this.Medical)
+            {
+                stringBuilder.AppendLine("MedicalBed".Translate());
+                stringBuilder.AppendLine("RoomSurgerySuccessChanceFactor".Translate() + ": " + this.GetRoom(RegionType.Set_Passable).GetStat(RoomStatDefOf.SurgerySuccessChanceFactor).ToStringPercent());
+                stringBuilder.AppendLine("RoomInfectionChanceFactor".Translate() + ": " + this.GetRoom(RegionType.Set_Passable).GetStat(RoomStatDefOf.InfectionChanceFactor).ToStringPercent());
+            }
+            else if (this.PlayerCanSeeOwners)
             {
                 if (this.owners.Count == 0)
                 {
@@ -104,7 +130,70 @@ namespace AssignAnimalBeds
             return stringBuilder.ToString().TrimEndNewlines();
         }
 
-        // NOTE: copy pasta from Building_Bed
+        /*public override IEnumerable<FloatMenuOption> GetFloatMenuOptions(Pawn myPawn)
+        {
+            if (!myPawn.RaceProps.Humanlike && this.Medical)
+            {
+                if (!HealthAIUtility.ShouldSeekMedicalRest(myPawn) && !HealthAIUtility.ShouldSeekMedicalRestUrgent(myPawn))
+                {
+                    yield return new FloatMenuOption("UseMedicalBed".Translate() + " (" + "NotInjured".Translate() + ")", null, MenuOptionPriority.Default, null, null, 0f, null, null);
+                }
+                else
+                {
+                    Action sleep = delegate
+                    {
+                        if (this.Medical && myPawn.CanReserveAndReach(this, PathEndMode.ClosestTouch, Danger.Deadly, this.SleepingSlotsCount, -1, null, true))
+                        {
+                            Job job = new Job(JobDefOf.LayDown, this) { restUntilHealed = true };
+                            myPawn.jobs.TryTakeOrderedJob(job, JobTag.Misc);
+                            myPawn.mindState.ResetLastDisturbanceTick();
+                        }
+                    };
+                    if (this.AnyUnoccupiedSleepingSlot)
+                    {
+                        yield return FloatMenuUtility.DecoratePrioritizedTask(new FloatMenuOption("UseMedicalBed".Translate(), sleep, MenuOptionPriority.Default, null, null, 0f, null, null), myPawn, this, "ReservedBy");
+                    }
+                    else
+                    {
+                        yield return FloatMenuUtility.DecoratePrioritizedTask(new FloatMenuOption("UseMedicalBed".Translate(), sleep, MenuOptionPriority.Default, null, null, 0f, null, null), myPawn, this, "SomeoneElseSleeping");
+                    }
+                }
+            }
+        }*/
+
+        public override void DrawGUIOverlay()
+        {
+            if (Find.CameraDriver.CurrentZoom == CameraZoomRange.Closest && this.PlayerCanSeeOwners)
+            {
+                Color defaultThingLabelColor = GenMapUI.DefaultThingLabelColor;
+                /*if (!this.owners.Any<Pawn>())
+                {
+                    GenMapUI.DrawThingLabel(this, "Unowned".Translate(), defaultThingLabelColor);
+                }
+                else */ // NOTE: only show owned.
+                if (this.owners.Count == 1)
+                {
+                    if (this.owners[0].InBed() && this.owners[0].CurrentBed() == this) 
+                    {
+                        return;
+                    }
+                    GenMapUI.DrawThingLabel(this, this.owners[0].NameStringShort, defaultThingLabelColor);
+                }
+                // NOTE: use this code later for multi.
+                /*else
+                {
+                    for (int i = 0; i < this.owners.Count; i++)
+                    {
+                        if (!this.owners[i].InBed() || this.owners[i].CurrentBed() != this || !(this.owners[i].Position == this.GetSleepingSlotPos(i)))
+                        {
+                            Vector3 multiOwnersLabelScreenPosFor = this.GetMultiOwnersLabelScreenPosFor(i);
+                            GenMapUI.DrawThingLabel(multiOwnersLabelScreenPosFor, this.owners[i].NameStringShort, defaultThingLabelColor);
+                        }
+                    }
+                }*/
+            }
+        }
+
         private bool PlayerCanSeeOwners
         {
             get
@@ -121,6 +210,32 @@ namespace AssignAnimalBeds
                     }
                 }
                 return false;
+            }
+        }
+
+        private FieldInfo FI_medicalInt = AccessTools.Field(typeof(Building_Bed), "medicalInt");
+        public new bool Medical
+        {
+            get
+            {
+                return (bool)FI_medicalInt.GetValue(this);
+            }
+            set
+            {
+                if (value == this.Medical || this.def.building.bed_humanlike)
+                {
+                    return;
+                }
+                Traverse t = Traverse.Create(this);
+                t.Method("RemoveAllOwners").GetValue(); //this.RemoveAllOwners();
+                FI_medicalInt.SetValue(this , value);
+                this.Notify_ColorChanged();
+                if (base.Spawned)
+                {
+                    base.Map.mapDrawer.MapMeshDirty(base.Position, MapMeshFlag.Things);
+                    t.Method("NotifyRoomBedTypeChanged").GetValue(); //this.NotifyRoomBedTypeChanged();
+                }
+                t.Method("FacilityChanged").GetValue(); //this.FacilityChanged();
             }
         }
 
---
+++ Source/AssignAnimalBeds/HarmonyPatches.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Runtime.CompilerServices;
+using Verse;
+using RimWorld;
+using Harmony;
+
+namespace AssignAnimalBeds
+{
+    /*[StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static BindingFlags bf = BindingFlags.NonPublic | BindingFlags.Instance;
+        static string varName = "pawn";
+        static Type anonType = typeof(RestUtility).GetNestedTypes(BindingFlags.NonPublic).First(t => t.HasAttribute<CompilerGeneratedAttribute>() && t.GetField(varName, bf) != null);
+        static MethodInfo anonMethod = anonType.GetMethods(bf).First(); // assuming first for now...
+
+        static HarmonyPatches()
+        {
+            HarmonyInstance harmony = HarmonyInstance.Create("rimworld.whyisthat.assignanimalbeds");
+            //Log.Message("anonMethod: " + anonMethod.Name);
+            harmony.Patch(anonMethod, null, null, new HarmonyMethod(typeof(HarmonyPatches).GetMethod(nameof(Transpiler))));
+        }
+
+        private static MethodInfo MI_getMedical = typeof(Building_Bed).GetProperty(nameof(Building_Bed.Medical)).GetGetMethod();
+        public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+        {
+            List<CodeInstruction> instructionList = instructions.ToList<CodeInstruction>();
+            int i;
+            for (i = 0; i < instructionList.Count; i++)
+            {
+                yield return instructionList[i];
+                if (instructionList[i].opcode == OpCodes.Callvirt && instructionList[i].operand == MI_getMedical)
+                {
+                    yield return instructionList[++i];
+                    while (instructionList[++i].opcode != OpCodes.Brfalse) { } // eat up instructions
+                    yield return new CodeInstruction(OpCodes.Ldc_I4_0); // force the break (which is really a continue in the statement)
+                    yield return instructionList[i];
+                }
+            }
+        }
+    }*/
+}
