7ff29d0be944ed79b95b00c2bcbdc22863c40857
XeoNovaDan <toghill7252010@hotmail.co.uk>
2018-09-01 20:41:50 +0100

    basic functionality w/ B19

---
+++ Source/AssignAnimalBeds/HarmonyPatches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Harmony;
+using RimWorld;
+
+namespace AssignAnimalBeds
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+
+        static HarmonyPatches()
+        {
+            HarmonyInstance h = HarmonyInstance.Create("WhyIsThat.AssignAnimalBeds");
+
+            h.Patch(AccessTools.Method(typeof(PawnComponentsUtility), nameof(PawnComponentsUtility.CreateInitialComponents)),
+                postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(Postfix_CreateInitialComponents)));
+        }
+
+        public static void Postfix_CreateInitialComponents(Pawn pawn)
+        {
+            if (pawn.RaceProps.Animal && pawn.ownership == null)
+                pawn.ownership = new Pawn_Ownership(pawn);
+        }
+
+    }
+}
